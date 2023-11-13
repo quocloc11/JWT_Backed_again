be996408ea2103c13e70cf742f773694ef315edb
unknown <loc842211@gmail.com>
2023-11-13 14:18:41 +0700

    init project hello work

---
+++ src/routes/web.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const router = express.Router()
+
+const initWebRoutes = (app) => {
+    router.get("/", (req, res) => {
+        return res.send("hello word")
+    })
+
+    return app.use("/", router);
+}
+
+export default initWebRoutes
