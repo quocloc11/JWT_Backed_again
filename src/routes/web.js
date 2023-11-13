e8282d9899beb565bfa2257c303f185bf0c5f570
unknown <loc842211@gmail.com>
2023-11-13 14:55:43 +0700

    ap dung mo hinh mvc

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from "express"
-
+import homeController from '../controller/homeCotroller'
 const router = express.Router()
 
+
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("hello word")
-    })
+
+    router.get("/user", homeController.handleUserPage)
+    router.get("/", homeController.handleHelloword)
 
     return app.use("/", router);
 }
