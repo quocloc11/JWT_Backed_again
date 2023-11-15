8d653dd2cd9161534ef2d3ea00a506bc3052a0b5
unknown <loc842211@gmail.com>
2023-11-15 13:04:38 +0700

    update

--- src/service/userService.js
+++ src/service/userService.js
@@ -59,6 +59,31 @@ const deleteUser = async (id) => {
     }
 
 }
+const getUserById = async (id) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+
+    try {
+        const [rows, fields] = await connection.execute('Select * FROM users WHERE id=?', [id]);
+        console.log('reow', rows)
+        return rows
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+const updateUserInfor = async (email, username, id) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+
+    try {
+        const [rows, fields] = await connection.execute(' UPDATE users SET email= ? , username= ? WHERE id=?', [email, username, id]);
+
+        return rows
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 module.exports = {
-    createNewUser, getUserList, deleteUser
+    createNewUser, getUserList, deleteUser, getUserById, updateUserInfor
 }
