dd9461440c801c9b57f0bff95cbb8a1c6627d92f
unknown <loc842211@gmail.com>
2023-11-18 20:18:51 +0700

    fix CORS

--- src/server.js
+++ src/server.js
@@ -15,6 +15,16 @@ configViewEngine(app)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// fix loi CORS 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL)
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    next()
+})
+
 //test connetion
 connection()
 
