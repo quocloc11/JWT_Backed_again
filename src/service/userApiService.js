a96da7ca9266da043ee067c4f4f2839bdbd129cf
unknown <loc842211@gmail.com>
2023-11-23 17:28:20 +0700

    create new user

--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -62,9 +62,12 @@ const getUserWithPagination = async (page, limit) => {
 }
 const createNewUser = async (data) => {
     try {
-        await db.User.create({
-
-        })
+        await db.User.create(data)
+        return {
+            EM: "OK",
+            EC: 0,
+            DT: [],
+        }
     } catch (e) {
         console.log(e)
     }
