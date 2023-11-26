f226d9e4652badc78cde17fcf48efcd48cf7841c
unknown <loc842211@gmail.com>
2023-11-26 23:09:24 +0700

    check quyen cho tat ca Routes

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 import jwt from "jsonwebtoken"
 
+const nonSecurePaths = ['/', '/login', '/register']
+
 const createJWT = (payload) => {
     let key = process.env.JWT_SECRET;
     let token = null
@@ -27,6 +29,8 @@ const verifyToken = (token) => {
 
 }
 const checkUserJWT = (req, res, next) => {
+    if (nonSecurePaths.includes(req.path)) return next()
+
     let cookies = req.cookies
     if (cookies && cookies.jwt) {
         let token = cookies.jwt
@@ -50,6 +54,8 @@ const checkUserJWT = (req, res, next) => {
     }
 }
 const checkUserPermission = (req, res, next) => {
+    if (nonSecurePaths.includes(req.path)) return next()
+
     if (req.user) {
         let email = req.user.email
         let roles = req.user.groupWithRoles.Roles
--- src/routes/api.js
+++ src/routes/api.js
@@ -6,14 +6,15 @@ import { checkUserJWT, checkUserPermission } from '../middleware/JWTAction'
 const router = express.Router()
 
 
-
 const initApiRoutes = (app) => {
+
+    router.all('*', checkUserJWT, checkUserPermission,)
 
     router.get("/test-api", apiController.testApi)
     router.post("/register", apiController.handleRegister)
     router.post("/login", apiController.handleLogin)
 
-    router.get("/user/read", checkUserJWT, checkUserPermission, userController.readFunc)
+    router.get("/user/read", userController.readFunc)
     router.post("/user/create", userController.createFunc)
     router.put("/user/update", userController.updateFunc)
     router.delete("/user/delete", userController.deleteFunc)
