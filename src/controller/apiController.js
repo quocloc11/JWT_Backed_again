a2d8f3fce38c47b4b19a1bd9e2260db17a6fe2eb
unknown <loc842211@gmail.com>
2023-11-27 12:50:21 +0700

    react context luu thong tin Global

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -42,7 +42,10 @@ const handleLogin = async (req, res) => {
     try {
         let data = await loginRegisterService.handleUserLogin(req.body)
 
-        res.cookie("jwt", data.DT.access_token, { httpOnly: true, maxAge: 60 * 60 * 1000 })
+        if (data && data.DT && data.DT.access_token) {
+
+            res.cookie("jwt", data.DT.access_token, { httpOnly: true, maxAge: 60 * 60 * 1000 })
+        }
         return res.status(200).json({
             EM: data.EM,
             EC: data.EC,
