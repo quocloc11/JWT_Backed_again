a9c6bf7c5fa7419e4d6367cdefdea4dc24e76aad
unknown <loc842211@gmail.com>
2023-11-28 23:01:57 +0700

    xac thuc Header Token

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,9 +5,12 @@ const confidCors = (app) => {
     app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL)
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
         res.setHeader('Access-Control-Allow-Credentials', true)
 
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200)
+        }
         next()
     })
 
--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -28,12 +28,23 @@ const verifyToken = (token) => {
 
 
 }
+
+const extractToken = (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1]
+    }
+    return null
+
+}
+
 const checkUserJWT = (req, res, next) => {
     if (nonSecurePaths.includes(req.path)) return next()
 
     let cookies = req.cookies
-    if (cookies && cookies.jwt) {
-        let token = cookies.jwt
+    let tokenFromHeader = extractToken(req)
+
+    if ((cookies && cookies.jwt) || tokenFromHeader) {
+        let token = cookies && cookies.jwt ? cookies.jwt : tokenFromHeader
         let decoded = verifyToken(token)
         if (decoded) {
             req.user = decoded;
@@ -46,7 +57,10 @@ const checkUserJWT = (req, res, next) => {
                 EM: 'Not authenticated the user'
             })
         }
-    } else {
+    }
+
+
+    else {
         return res.status(401).json({
             EC: -1,
             DT: '',
